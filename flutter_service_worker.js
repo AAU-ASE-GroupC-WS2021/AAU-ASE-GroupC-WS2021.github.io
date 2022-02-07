a73c54d7799f12b90187bc17dd8bc3582e485241
CircleCI <ci@circleci.com>
2022-02-07 15:59:42 +0000

    Merge pull request #32 from AAU-ASE-GroupC-WS2021/feature/sign-up
    built in CircleCI from AAU-ASE-GroupC-WS2021/canteenMgmtFrontend@2b9ce0f7bd0bbfbeebaa6f565cbb0e2d88e1b5ad

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5f67f677af7d76ac5b2d48a8ade355d8",
-"/": "5f67f677af7d76ac5b2d48a8ade355d8",
+"index.html": "24ff821a0725742ab54dc1df8fc62fa9",
+"/": "24ff821a0725742ab54dc1df8fc62fa9",
 "manifest.json": "ad04df876676be4d6f04f5d81b1c75ec"
 };
 
